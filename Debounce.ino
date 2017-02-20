283551e8623759dee29c6fe766d45ed23e3a99e8
Bailey Smith <s00171876@e0033-08.campus.itsligo.ie>
2017-02-20 14:56:13 +0000

    Add Debounce.ino file

---
+++ Debounce.ino
@@ -0,0 +1,208 @@
+/*
+
+ Debounce
+
+
+
+ Each time the input pin goes from LOW to HIGH (e.g. because of a push-button
+
+ press), the output pin is toggled from LOW to HIGH or HIGH to LOW.  There's
+
+ a minimum delay between toggles to debounce the circuit (i.e. to ignore
+
+ noise).
+
+
+
+ The circuit:
+
+ * LED attached from pin 13 to ground
+
+ * pushbutton attached from pin 2 to +5V
+
+ * 10K resistor attached from pin 2 to ground
+
+
+
+ * Note: On most Arduino boards, there is already an LED on the board
+
+ connected to pin 13, so you don't need any extra components for this example.
+
+
+
+
+
+ created 21 November 2006
+
+ by David A. Mellis
+
+ modified 30 Aug 2011
+
+ by Limor Fried
+
+ modified 28 Dec 2012
+
+ by Mike Walters
+
+
+
+ This example code is in the public domain.
+
+
+
+ http://www.arduino.cc/en/Tutorial/Debounce
+
+ */
+
+
+
+
+// constants won't change. They're used here to
+
+// set pin numbers:
+
+const int buttonPin = 2;    // the number of the pushbutton pin
+
+const int ledPin = 4;      // the number of the LED pin
+
+
+
+
+// Variables will change:
+
+int ledState = HIGH;         // the current state of the output pin
+
+int buttonState;             // the current reading from the input pin
+
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+
+
+
+// the following variables are long's because the time, measured in miliseconds,
+
+// will quickly become a bigger number than can be stored in an int.
+
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+
+
+
+void setup() {
+
+  pinMode(buttonPin, INPUT);
+
+  pinMode(ledPin, OUTPUT);
+
+
+
+
+  // set initial LED state
+
+  digitalWrite(ledPin, ledState);
+
+}
+
+
+
+
+void loop() {
+
+  // read the state of the switch into a local variable:
+
+  int reading = digitalRead(buttonPin);
+
+
+
+
+  // check to see if you just pressed the button
+
+  // (i.e. the input went from LOW to HIGH),  and you've waited
+
+  // long enough since the last press to ignore any noise:
+
+
+
+
+  // If the switch changed, due to noise or pressing:
+
+  if (reading != lastButtonState) {
+
+    // reset the debouncing timer
+
+    lastDebounceTime = millis();
+
+  }
+
+
+
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+
+    // whatever the reading is at, it's been there for longer
+
+    // than the debounce delay, so take it as the actual current state:
+
+
+
+
+    // if the button state has changed:
+
+    if (reading != buttonState) {
+
+      buttonState = reading;
+
+
+
+
+      // only toggle the LED if the new button state is HIGH
+
+      if (buttonState == HIGH) {
+
+        ledState = !ledState;
+
+      }
+
+    }
+
+  }
+
+
+
+
+  // set the LED:
+
+  digitalWrite(ledPin, ledState);
+
+
+
+
+  // save the reading.  Next time through the loop,
+
+  // it'll be the lastButtonState:
+
+  lastButtonState = reading;
+
+}
+
+/*
+ * The code is telling the arduino to change how much voltage is being applied to the LED on pin 13 (whether to turn it ON or OFF) 
+ * when the button on pin 2 is pressed 
+ * 
+ * Main Features
+ *``````````````````````````
+ *  - Constants are being declared at the top of the program to ensure that the LED pin and button pins don't change
+ *  - Variables are being set to indicate wether:
+ *    - the LED is on or OFF
+ *    - the button has been has been pressed
+ *    - the button as changed state
+ *    - the time intervals of the state change on the button and LED
+ *  - Some IF statements to see if the button has been pressed
+ *  - if the button state is currently different to what is previously was
+ *  - if the voltage being applied to the LED should change
+ *  
+ *  The LED is on pin13
+ *  the button is on pin2
+ */
